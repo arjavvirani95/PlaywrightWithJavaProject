9dc6f9b02c97d2f302c8f849ec52b5ad5c5c9535
Arjavkumar virani <arjav.virani17@gmail.com>
2024-03-15 16:09:12 -0400

    tried with different browser chrome firefox webkit chromium

--- src/test/java/PlaywrightWithJava/AppTest.java
+++ src/test/java/PlaywrightWithJava/AppTest.java
@@ -1,13 +1,17 @@
 package PlaywrightWithJava;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.BrowserType.LaunchOptions;
 
 public class AppTest 
 {
 	public static void main(String[] args) {
 		try (Playwright playwright = Playwright.create()) //It will start playwright server which is in nodejs
 		{
-			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false)); //it will launch the browser
+			LaunchOptions lp = new LaunchOptions();
+			lp.setChannel("chrome");
+			lp.setHeadless(false);
+			Browser browser = playwright.chromium().launch(lp); //it will launch the browser
 			Page page = browser.newPage(); //
 			page.navigate("http://Google.com");
 			System.out.println(page.title());
